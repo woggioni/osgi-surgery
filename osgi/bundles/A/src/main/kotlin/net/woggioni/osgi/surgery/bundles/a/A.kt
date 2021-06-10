d5c4c435db07bc0644269cdd777ac74b1735e0a4
Walter Oggioni <oggioni.walter@gmail.com>
2021-06-10 14:23:05 +0100

    reproduced bug using Bndrun task

--- osgi/bundles/A/src/main/kotlin/net/woggioni/osgi/surgery/bundles/a/A.kt
+++ osgi/bundles/A/src/main/kotlin/net/woggioni/osgi/surgery/bundles/a/A.kt
@@ -3,8 +3,6 @@ package net.woggioni.osgi.surgery.bundles.a
 import net.woggioni.osgi.surgery.bundles.b.KotlinChild
 import org.osgi.framework.BundleActivator
 import org.osgi.framework.BundleContext
-import org.slf4j.LoggerFactory
-import kotlin.reflect.full.memberProperties
 
 class Bar : KotlinChild {
     override fun foo() {
@@ -17,13 +15,10 @@ class Bar : KotlinChild {
 }
 
 class Activator : BundleActivator {
-    companion object {
-        private val log = LoggerFactory.getLogger(BundleActivator::class.java)
-    }
 
     override fun start(context: BundleContext?) {
         val bar = Bar()
-        println(bar::class.memberProperties)
+        println(bar::class.members)
     }
 
     override fun stop(context: BundleContext?) {}
